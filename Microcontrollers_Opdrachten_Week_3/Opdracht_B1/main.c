75dda112cea4573f34097e7058f11809f4e1212b
Kenley <kenley.strik@casema.nl>
2020-02-19 12:51:26 +0100

    Added solution for week 3 opdrachten

---
+++ Microcontrollers_Opdrachten_Week_3/Opdracht_B1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht_B1.c
+ *
+ * Created: 19/02/2020 12:51:00
+ * Author : Kenley
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
