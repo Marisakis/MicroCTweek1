b0b4035d362e40625d40d2d52f152bebd9f57bdd
Kenley <kenley.strik@casema.nl>
2020-02-19 14:07:54 +0100

    Added project Opdracht_B2 to week 3

---
+++ Microcontrollers_Opdrachten_Week_3/Opdracht_B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht_B2.c
+ *
+ * Created: 19/02/2020 14:07:38
+ * Author : Kenley
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
