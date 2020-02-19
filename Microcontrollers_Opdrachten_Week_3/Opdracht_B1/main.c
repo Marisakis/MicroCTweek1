511dbb20c6d24139d1b98b8009a4bca670a66d37
Kenley <kenley.strik@casema.nl>
2020-02-19 13:40:46 +0100

    Changes to Opdracht_B1 week 3

--- Microcontrollers_Opdrachten_Week_3/Opdracht_B1/main.c
+++ Microcontrollers_Opdrachten_Week_3/Opdracht_B1/main.c
@@ -1,18 +1,48 @@
-/*
- * Opdracht_B1.c
- *
- * Created: 19/02/2020 12:51:00
- * Author : Kenley
- */ 
+#define F_CPU 16e6
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 
-int main(void)
+void wait( int ms ) 
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	for (int i = 0; i < ms; i++) 
+		_delay_ms( 1 );
+}
+
+void setupTimer(void) {
+
+	cli();
+
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCNT1  = 0;
+	
+	OCR1A = 10000;
+	
+	TCCR1B |= (1 << WGM12);
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	TIMSK |= (1 << OCIE1A);
+
+	sei();
+}
+
+
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTB ^= (1<<7);
+}
+
+
+int main(void) 
+{
+	DDRB = 0xFF;
+	setupTimer();
+	while (1)  {
+		wait(50);
+	}
+	return 0;
 }
 
