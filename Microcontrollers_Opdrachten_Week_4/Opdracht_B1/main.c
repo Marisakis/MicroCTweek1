79431dedc60393dbdbfb40090601caf32e7bced8
Marisakis <sebcolijn@live.nl>
2020-03-04 13:37:55 +0100

    Opdr B1

--- Microcontrollers_Opdrachten_Week_4/Opdracht_B1/main.c
+++ Microcontrollers_Opdrachten_Week_4/Opdracht_B1/main.c
@@ -1,18 +1,61 @@
 /*
- * Opdracht_B1.c
- *
- * Created: 04/03/2020 12:16:03
- * Author : Kenley
- */ 
+ * Project name:
+     Demo4_1 : Free running AD conversion on channel 1, PORTF.1
+ * Author: Avans-TI, Sebastiaan Colijn
+ * Revision History: 
+     20101229: - initial release;
+	 20200304 - edited
+ * Description:
+     This program gives an interrupt on each ms
+ * Test configuration:
+     MCU:             ATmega128
+     Dev.Board:       BIGAVR6
+     Oscillator:      External Clock 08.0000 MHz
+     Ext. Modules:    -
+     SW:              AVR-GCC
+ * NOTES:
+     - Turn ON the PORT LEDs at SW12.1 - SW12.8
+*/
+
+#define F_CPU 80000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+#define BIT(x)	(1 << (x))
 
-int main(void)
+// wait(): busy waiting for 'ms' millisecond
+// Used library: util/delay.h
+void wait( int ms )
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	for (int tms=0; tms<ms; tms++)
+	{
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
 }
 
+
+// Initialize ADC: 10-bits (left justified), free running
+void adcInit( void )
+{
+	ADMUX = 0b00100000;			// AREF=2.56, result left adjusted, channel1 at pin PF0
+	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+}
+
+
+// Main program: ADC at PF1
+int main( void )
+{
+	DDRF = 0x00;				// set PORTF for input (ADC)
+	DDRA = 0xFF;				// set PORTA for output 
+	DDRB = 0xFF;				// set PORTB for output
+	adcInit();					// initialize ADC
+
+	while (1)
+	{
+		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
+		PORTA = ADCH;
+		wait(100);				// every 100 ms (busy waiting)
+	}
+}
